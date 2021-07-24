e20c4e61d8717edb6f6980de548ada2bd432184b
Chedly Zouche <chedlyzouche9999@outlook.com>
2021-07-24 08:53:41 +0100

    Added some packages and fixed others

--- post-reboot.sh
+++ post-reboot.sh
@@ -18,7 +18,7 @@ sudo pacman -S nvidia nvidia-utils nvidia-settings
 sudo pacman -S alacritty nautilus ranger ueberzug feh
 
 # Tools
-sudo pacman -S lxappearance arandr playerctl flameshot gsimplecal
+sudo pacman -S lxappearance arandr playerctl flameshot gsimplecal discord
 
 # PDF
 sudo pacman -S evince zathura zathura-cb zathura-djvu zathura-pdf-mupdf zathura-ps
@@ -43,12 +43,12 @@ paru -S skippy-xd-git dunst
 sudo pacman -S libappindicator-gtk2 libappindicator-gtk3
 
 # Display manager
-#paru -S ly 
-#sudo systemctl enable ly.service
+paru -S ly 
+sudo systemctl enable ly.service
 
 # Wayland
-#sudo pacman -S sway swaylock swayidle xorg-xwayland xdg-desktop-portal-wlr qt5-wayland  wl-clipboard swappy grim slurp libnotify dunst glfw-wayland xorg-xlsclients xdg-desktop-portal-wlr libpipewire02 polkit-gnome
-#paru -S wdisplays sway-desktop-launcher
+#sudo pacman -S sway swaylock swayidle xorg-xwayland waybar xdg-desktop-portal-wlr qt5-wayland  wl-clipboard swappy grim slurp libnotify dunst glfw-wayland xorg-xlsclients xdg-desktop-portal-wlr libpipewire02 polkit-gnome
+#paru -S wdisplays sway-launcher-desktop
 
 # Media
 sudo pacman -S mpv youtube-dl vlc
@@ -66,7 +66,7 @@ sudo pacman -S gnome-disk-utility dosfstools ntfs-3g btrfs-progs exfatutils gptf
 sudo pacman -S net-tools bind-tools traceroute rsync wget
 
 # Pulse audio
-sudo pacman -S pulseaudio pulsemixer alsa-utils alsa-plugins pavucontrol easyeffects 
+sudo pacman -S pulseaudio pulsemixer alsa-utils alsa-plugins pavucontrol easyeffects volumeicon
 
 # Pipewire
 #sudo pacman -S alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack pavucontrol
@@ -76,6 +76,7 @@ paru -S ttf-ms-fonts ttf-monaco
 
 sudo pacman -S terminus-font noto-fonts noto-fonts-cjk ttf-dejavu ttf-liberation ttf-opensans gsfonts sdl_ttf ttf-bitstream-vera xorg-fonts-type1 gnu-free-fonts ttf-caladea ttf-carlito ttf-croscore ttf-hack ttf-junicode ttf-linux-libertine opendesktop-fonts ttf-anonymous-pro ttf-arphic-ukai ttf-arphic-uming ttf-baekmuk ttf-cascadia-code ttf-cormorant ttf-droid ttf-fantasque-sans-mono ttf-fira-code ttf-fira-sans ttf-fira-mono ttf-font-awesome ttf-hanazono ttf-ibm-plex ttf-hannom ttf-inconsolata ttf-indic-otf ttf-indic-otf ttf-ionicons ttf-jetbrains-mono ttf-joypixels ttf-khmer ttf-lato ttf-nerd-fonts-symbols ttf-opensans ttf-proggy-clean ttf-roboto adobe-source-code-pro-fonts ttf-ubuntu-font-family
 
-# System76-power for laptops with optimus
+# Laptops
+sudo pacman -S cbatticon
 paru -S system76-power
 sudo systemctl enable system76-power.service
